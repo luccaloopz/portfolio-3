63e2e050ddb23cca786fff7cc2fcb7201e183bef
luccaloopz <luccamartins2012@gmail.com>
2022-05-15 15:48:29 -0700

    added to and fixed footer

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +8,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
-
-// app.use(routes);
 
 app.get('/projects', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/pages/projects.html'))
