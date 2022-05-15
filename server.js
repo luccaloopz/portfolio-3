4194d1813c4e6511c7c140283c2d9ef4708aed6b
luccaloopz <luccamartins2012@gmail.com>
2022-05-15 13:47:42 -0700

    when user clicks on projects in the navigation or on the respective sentence in the statement, they're redirected to the projects page.

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.static('public'));
 
 // app.use(routes);
 
-app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get('/projects', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/pages/projects.html'))
 });
 
-app.use('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/projects.html'))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.listen(PORT, () => {
